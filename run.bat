c66833953558e35e1662c640cf8e1b4093ca578d
cilame <opaquism@hotmail.com>
2020-01-30 20:15:59 +0800

    add

--- run.bat
+++ run.bat
@@ -6,6 +6,12 @@ SET PATH=%PATH%;%~dp0tools\bin;%~dp0tools\tcc
 .\tools\bin\flex.exe c.l
 tcc -o a.exe y.tab.c lex.yy.c
 
+
 @echo off
-del y.tab.c y.tab.h y.output lex.yy.c
-@echo on
+if "%1"=="test" ( 
+ echo "use test model. keep middle file:y.tab.c y.tab.h y.output lex.yy.c"
+) else (
+ @echo off
+ del y.tab.c y.tab.h y.output lex.yy.c
+ @echo on
+)
--- test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET PATH=%PATH%;%~dp0tools\bin;%~dp0tools\tcc
-@echo on
-
-.\tools\bin\bison.exe -vdty c.y
-.\tools\bin\flex.exe c.l
-tcc -o a.exe y.tab.c lex.yy.c
